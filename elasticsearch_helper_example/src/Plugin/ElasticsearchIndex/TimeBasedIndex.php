3aa5daf2100b51a265a3828a037d905e9419b24b
Florian Lorétan <florian.loretan@wunderkraut.com>
2016-06-28 16:03:50 +0200

    Add an example of a time-based dynamic index.

---
+++ elasticsearch_helper_example/src/Plugin/ElasticsearchIndex/TimeBasedIndex.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\elasticsearch_helper_example\Plugin\ElasticsearchIndex;
+
+use Drupal\elasticsearch_helper\Annotation\ElasticsearchIndex;
+use Drupal\elasticsearch_helper\Plugin\ElasticsearchIndexBase;
+use Drupal\node\Entity\Node;
+
+/**
+ * @ElasticsearchIndex(
+ *   id = "time_based_index",
+ *   label = @Translation("Example Time-based Index"),
+ *   indexName = "time-based-{year}{month}",
+ *   typeName = "node",
+ *   entityType = "node"
+ * )
+ */
+class TimeBasedIndex extends ElasticsearchIndexBase {
+
+  public function serialize($source) {
+    /** @var Node $source */
+
+    $data = parent::serialize($source);
+
+    // Add the creation date.
+    $data['created'] = $source->getCreatedTime();
+
+    // Add attributes matching the placeholders in the indexName.
+    $data['year'] = date('Y', $source->getCreatedTime());
+    $data['month'] = date('m', $source->getCreatedTime());
+
+    return $data;
+  }
+  
+  /**
+   * @inheritdoc
+   */
+  public function setup() {
+    $this->client->indices()->putTemplate([
+      'name' => $this->pluginId,
+      'body' => [
+        // Any index matching the pattern will get the given index configuration.
+        'template' => $this->indexNamePattern(),
+        'mappings' => [
+          'node' => [
+            'properties' => [
+              'created' => [
+                'type' => 'date',
+                'format' => 'epoch_second',
+              ],
+              // Don't save year and month, we just need them for the placeholders.
+              'year' => [
+                'enabled' => FALSE,
+              ],
+              'month' => [
+                'enabled' => FALSE,
+              ],
+            ]
+          ]
+        ]
+      ]
+    ]);
+  }
+  
+}
