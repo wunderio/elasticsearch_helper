c3469fd4be7a4b12b40237ee6bdc2a2c9a590bd6
maijs <raimonds.kalnins@gmail.com>
2017-03-27 17:38:24 +0300

    An option to print Elasticsearch query to browser console added. Query is printed to console only if user has permission "administer views" and if "Show the SQL query" option is selected in Views settings.

---
+++ src/Ajax/ElasticsearchDebugCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\elasticsearch_helper_views\Ajax;
+
+use Drupal\Core\Ajax\CommandInterface;
+
+/**
+ * Prints Elasticsearch query to console for debugging.
+ *
+ * @ingroup ajax
+ */
+class ElasticsearchDebugCommand implements CommandInterface {
+
+  /** @var string $text */
+  protected $text;
+
+  /**
+   * ElasticsearchPrintToConsoleCommand constructor.
+   *
+   * @param $text
+   */
+  public function __construct($text) {
+    $this->text = $text;
+  }
+
+  /**
+   * Implements \Drupal\Core\Ajax\CommandInterface:render().
+   */
+  public function render() {
+    return array(
+      'command' => 'elasticsearch_query_debug',
+      'text' => $this->text,
+    );
+  }
+
+}
