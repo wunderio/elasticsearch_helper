41a7fc34332db96492233d6f50866d81924dcbc8
maijs <raimonds.kalnins@gmail.com>
2018-01-29 21:21:31 +0200

    Signature of query builder interface changed; methods do not require explicit Views object as it's already available via $this->view.

--- src/ElasticsearchQueryBuilderInterface.php
+++ src/ElasticsearchQueryBuilderInterface.php
@@ -4,7 +4,6 @@ namespace Drupal\elasticsearch_helper_views;
 
 use Drupal\Component\Plugin\PluginInspectionInterface;
 use Drupal\Core\Cache\CacheableDependencyInterface;
-use Drupal\views\ViewExecutable;
 
 /**
  * Defines an interface for Elasticsearch query builder plugins.
@@ -17,39 +16,32 @@ interface ElasticsearchQueryBuilderInterface extends PluginInspectionInterface, 
    * The query array needs to be compatible with
    * \Elasticsearch\Client::search().
    *
-   * @param ViewExecutable $view
-   *
    * @return array
    *
    * @see \Elasticsearch\Client::search()
    */
-  public function buildQuery(ViewExecutable $view);
+  public function buildQuery();
 
   /**
    * Returns filter values from a view.
    *
-   * @param \Drupal\views\ViewExecutable $view
-   *
    * @return array
    */
-  public function getFilterValues(ViewExecutable $view);
+  public function getFilterValues();
 
   /**
    * Returns argument values from a view.
    *
-   * @param \Drupal\views\ViewExecutable $view
-   *
    * @return array
    */
-  public function getArgumentValues(ViewExecutable $view);
+  public function getArgumentValues();
 
   /**
    * Returns sort values from a view.
    *
-   * @param \Drupal\views\ViewExecutable $view
-   *
    * @return array
+   *
    */
-  public function getSortValues(ViewExecutable $view);
+  public function getSortValues();
 
 }
