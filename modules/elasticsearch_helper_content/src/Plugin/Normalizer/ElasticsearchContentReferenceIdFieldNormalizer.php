2d56952bac13c1cdc1a251a1b4ab07bb78a3abef
Marc Galang <marc.galang@wunder.io>
2018-07-06 13:36:47 +0100

    Issue #9: Add entity reference field normalizers

---
+++ modules/elasticsearch_helper_content/src/Plugin/Normalizer/ElasticsearchContentReferenceIdFieldNormalizer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\elasticsearch_helper_content\Plugin\Normalizer;
+
+use Drupal\serialization\Normalizer\FieldNormalizer;
+use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
+
+/**
+ * Normalizes / denormalizes Fields
+ */
+class ElasticsearchContentReferenceIdFieldNormalizer extends FieldNormalizer {
+  /**
+   * Supported formats.
+   *
+   * @var array
+   */
+  protected $format = ['elasticsearch_helper'];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function normalize($object, $format = NULL, array $context = []) {
+    $attributes = [];
+
+    foreach ($object as $field_item) {
+      if ($field_item instanceof EntityReferenceItem) {
+        $entity = $field_item
+          ->get('entity')
+          ->getTarget()
+          ->getValue();
+
+        $attributes[] = ['id' => $entity->id()];
+      }
+    }
+
+    return $attributes;
+  }
+
+}
