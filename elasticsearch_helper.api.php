2d1ce262c70dc50dc142b2d3061c8a21c8a32066
Georg Oechsler <go@wunderkraut.com>
2018-10-18 14:54:21 +0200

    Adds and connects an IndexManager and ctool plugin infrastructure. Still buggy

--- elasticsearch_helper.api.php
+++ elasticsearch_helper.api.php
@@ -16,9 +16,21 @@
  * @param \Elasticsearch\ClientBuilder
  *   The ClientBuilder object.
  */
-function elasticsearch_helper_aws_elasticsearch_helper_client_builder_alter(\Elasticsearch\ClientBuilder $clientBuilder) {
+function hook_elasticsearch_helper_client_builder_alter($clientBuilder) {
   // Send log entries from the client directly to Drupal's log.
   $clientBuilder->setLogger(\Drupal::logger('elasticsearch'));
+}
+
+/**
+ * Allow altering a plugins after loading
+ *
+ * @param array plugin definition
+ * @param array plugin type info
+ */
+function hook_elasticsearch_helper_index_plugin_alter(&$plugin, &$info) {
+  if ($plugin['id'] == 'content_index_node') {
+    $plugin['serialize_callback'] = "my_custom_seralization_callback";
+  }
 }
 
 /**
---
+++ src/ElasticsearchIndexManager.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * Provides the Elasticsearch index plugin manager.
+ */
+class ElasticsearchIndexManager {
+
+  /**
+   * Index an entity into any matching indices.
+   */
+  public function indexEntity($entity, $type) {
+    ctools_include('plugins');
+    $plugins = ctools_get_plugins('elasticsearch_helper', 'elasticsearch_helper_index');
+
+    foreach ($plugins as $plugin) {
+      if (isset($plugin['entityType']) && $type == $plugin['entityType']) {
+        if (!empty($plugin['bundle']) && $plugin['bundle'] != $entity->bundle()) {
+          // Do not index if defined plugin bundle differs from entity bundle.
+          continue;
+        }
+
+        try {
+          if ($function = ctools_plugin_get_function($plugin, 'index_callback')) {
+            $function($entity, $plugin);
+          }
+
+        }
+        catch (Exception $e) {
+        watchdog('Elasticsearch indexing failed: @message', [
+            '@message' => $e->getMessage(),
+          ], WATCHDOG_ERROR);
+        }
+      }
+    }
+  }
+
+  /**
+   * Delete an entity from any matching indices.
+   */
+  public function deleteEntity($entity, $type) {
+    foreach ($this->getDefinitions() as $plugin) {
+      if (isset($plugin['entityType']) && $entity->getEntityTypeId() == $plugin['entityType']) {
+        if (!empty($plugin['bundle']) && $plugin['bundle'] != $entity->bundle()) {
+          // Do not delete if defined plugin bundle differs from entity bundle.
+          continue;
+        }
+
+        try {
+          // Delete the entity from elasticsearch.
+          $this->createInstance($plugin['id'])->delete($entity);
+        }
+        catch (ElasticsearchException $e) {
+          $this->logger->error('Elasticsearch deletion failed: @message', [
+            '@message' => $e->getMessage(),
+          ]);
+
+          // TODO: queue for later indexing.
+        }
+      }
+    }
+  }
+
+  /**
+   * Reindex elasticsearch with all entities.
+   */
+  public function reindex($indices = []) {
+
+    foreach ($this->getDefinitions() as $plugin) {
+      if (empty($indices) || in_array($plugin['id'], $indices)) {
+
+        if ($plugin['entityType']) {
+          $query = $this->entityTypeManager->getStorage($plugin['entityType'])->getQuery();
+
+          $entity_type = $this->entityTypeManager->getDefinition($plugin['entityType']);
+
+          if ($plugin['bundle']) {
+            $query->condition($entity_type->getKey('bundle'), $plugin['bundle']);
+          }
+
+          $result = $query->execute();
+
+          foreach ($result as $entity_id) {
+            $this->queue->createItem([
+              'entity_type' => $entity_type->id(),
+              'entity_id' => $entity_id,
+            ]);
+          }
+          $this->logger->notice("Marked indices to be reindex on next cronrun");
+        }
+      }
+    }
+  }
+
+}
