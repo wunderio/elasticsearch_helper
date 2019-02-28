5f375de7581a3d20d6044cee558762c3fd94e236
Raimonds Kalnins <raimonds.kalnins@wunder.io>
2019-02-28 12:17:56 +0200

    Obsolete files removed.

--- modules/elasticsearch_helper_content/src/Exception/ElasticsearchContentSettingsException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Drupal\elasticsearch_helper_content;
-
-/**
- * Exception thrown by ContentLanguageSettings when target bundle is not set.
- */
-class ElasticsearchContentSettingsException extends \RuntimeException {}
