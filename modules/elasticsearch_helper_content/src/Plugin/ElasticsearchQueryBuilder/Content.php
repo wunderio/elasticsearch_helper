36523e7a7626ee53c852efde839010c712867678
Raimonds Kalnins <raimonds.kalnins@wunder.io>
2019-03-01 17:24:40 +0200

    Elasticsearch content query builder plugin scaffold added.

---
+++ modules/elasticsearch_helper_content/src/Plugin/ElasticsearchQueryBuilder/Content.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\elasticsearch_helper_content\Plugin\ElasticsearchQueryBuilder;
+
+use Drupal\elasticsearch_helper_views\Plugin\ElasticsearchQueryBuilder\ElasticsearchQueryBuilderPluginBase;
+
+/**
+ * @ElasticsearchQueryBuilder(
+ *   id = "elasticsearch_content",
+ *   label = @Translation("Content"),
+ *   description = @Translation("Query builder for indices built by Elasticsearch Helper Content module")
+ * )
+ */
+class Content extends ElasticsearchQueryBuilderPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildQuery() {
+    $query = [
+      'index' => $this->getIndices(),
+      'body' => [
+      ],
+    ];
+
+    return $query;
+  }
+
+  /**
+   * Returns a list of indices ("elasticsearch_index" filter value).
+   *
+   * @return array
+   */
+  protected function getIndices() {
+    // Get filter values.
+    $values = $this->getFilterValues();
+
+    if (isset($values['elasticsearch_index'])) {
+      return $values['elasticsearch_index'];
+    }
+
+    return [];
+  }
+
+}
