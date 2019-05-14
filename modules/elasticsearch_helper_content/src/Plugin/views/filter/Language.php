3443c30fa0c87a3b5955a272546bd2eaaa40352a
Raimonds Kalnins <raimonds.kalnins@wunder.io>
2019-05-14 15:22:08 +0300

    Language views filter plugin added.

---
+++ modules/elasticsearch_helper_content/src/Plugin/views/filter/Language.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\elasticsearch_helper_content\Plugin\views\filter;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\views\Plugin\views\filter\InOperator;
+use Drupal\views\Plugin\views\PluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides filtering by language.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("elasticsearch_content_language")
+ */
+class Language extends InOperator implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * Language constructor.
+   *
+   * @param $configuration
+   * @param $plugin_id
+   * @param $plugin_definition
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   */
+  public function __construct($configuration, $plugin_id, $plugin_definition, LanguageManagerInterface $language_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->languageManager = $language_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('language_manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getValueOptions() {
+    if (!isset($this->valueOptions)) {
+      $this->valueTitle = $this->t('Language');
+      // Pass the current values so options that are already selected do not get
+      // lost when there are changes in the language configuration.
+      $this->valueOptions = $this->listLanguages(LanguageInterface::STATE_ALL | LanguageInterface::STATE_SITE_DEFAULT | PluginBase::INCLUDE_NEGOTIATED, array_keys($this->value));
+    }
+    return $this->valueOptions;
+  }
+
+}
