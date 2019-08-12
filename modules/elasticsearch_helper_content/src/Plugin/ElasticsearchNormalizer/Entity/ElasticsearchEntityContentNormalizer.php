aabf0acd044feb6ad0c22bf7a041e50869bdeedb
Raimonds Kalnins <raimonds.kalnins@wunder.io>
2019-08-12 15:55:12 +0300

    Elasticsearch normalizer helper service class added.

--- modules/elasticsearch_helper_content/src/Plugin/ElasticsearchNormalizer/Entity/ElasticsearchEntityContentNormalizer.php
+++ modules/elasticsearch_helper_content/src/Plugin/ElasticsearchNormalizer/Entity/ElasticsearchEntityContentNormalizer.php
@@ -5,6 +5,7 @@ namespace Drupal\elasticsearch_helper_content\Plugin\ElasticsearchNormalizer\Ent
 use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\elasticsearch_helper_content\ElasticsearchNormalizerHelper;
 use Drupal\elasticsearch_helper_content\EntityRendererInterface;
 use Drupal\elasticsearch_helper_content\ElasticsearchDataTypeDefinition;
 use Drupal\elasticsearch_helper_content\ElasticsearchEntityNormalizerBase;
@@ -35,6 +36,11 @@ class ElasticsearchEntityContentNormalizer extends ElasticsearchEntityNormalizer
   protected $entityRenderer;
 
   /**
+   * @var \Drupal\elasticsearch_helper_content\ElasticsearchNormalizerHelper
+   */
+  protected $normalizerHelper;
+
+  /**
    * ElasticsearchEntityContentNormalizer constructor.
    *
    * @param array $configuration
@@ -44,12 +50,13 @@ class ElasticsearchEntityContentNormalizer extends ElasticsearchEntityNormalizer
    * @param \Drupal\Core\Entity\EntityDisplayRepositoryInterface $entity_display_repository
    * @param \Drupal\elasticsearch_helper_content\EntityRendererInterface $entity_renderer
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, EntityDisplayRepositoryInterface $entity_display_repository, EntityRendererInterface $entity_renderer) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, EntityDisplayRepositoryInterface $entity_display_repository, EntityRendererInterface $entity_renderer, ElasticsearchNormalizerHelper $normalizer_helper) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
 
     $this->entityTypeManager = $entity_type_manager;
     $this->entityDisplayRepository = $entity_display_repository;
     $this->entityRenderer = $entity_renderer;
+    $this->normalizerHelper = $normalizer_helper;
   }
 
   /**
@@ -62,7 +69,8 @@ class ElasticsearchEntityContentNormalizer extends ElasticsearchEntityNormalizer
       $plugin_definition,
       $container->get('entity_type.manager'),
       $container->get('entity_display.repository'),
-      $container->get('elasticsearch_helper_content.entity_renderer')
+      $container->get('elasticsearch_helper_content.entity_renderer'),
+      $container->get('elasticsearch_helper_content.normalizer_helper')
     );
   }
 
@@ -123,7 +131,7 @@ class ElasticsearchEntityContentNormalizer extends ElasticsearchEntityNormalizer
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    $entity_view_displays = $this->getEntityViewDisplayOptions();
+    $entity_view_displays = $this->normalizerHelper->getEntityViewDisplayOptions($this->configuration['entity_type'], $this->configuration['bundle']);
 
     return [
       '#tree' => TRUE,
@@ -142,49 +150,6 @@ class ElasticsearchEntityContentNormalizer extends ElasticsearchEntityNormalizer
         ],
       ],
     ];
-  }
-
-  /**
-   * Returns a list of enabled view modes.
-   *
-   * @return array
-   */
-  protected function getEntityViewDisplayOptions() {
-    $view_modes = [];
-
-    try {
-      // Get all view modes.
-      $view_modes = $this->entityDisplayRepository->getViewModes($this->configuration['entity_type']);
-
-      // Get entity view display IDs (enabled view modes).
-      $entity_view_display_storage = $this->entityTypeManager->getStorage('entity_view_display');
-      $query = $entity_view_display_storage->getQuery()
-        ->condition('targetEntityType', $this->configuration['entity_type'])
-        ->condition('bundle', $this->configuration['bundle'])
-        ->condition('status', TRUE);
-
-      // Get the view modes from retrieved entity view displays.
-      $enabled_view_mode_ids = [];
-      if ($entity_view_display_result = $query->execute()) {
-        $enabled_view_mode_ids = array_map(function ($entity) {
-          /** @var \Drupal\Core\Entity\Entity\EntityViewDisplay $entity */
-          return $entity->getMode();
-        }, $entity_view_display_storage->loadMultiple($entity_view_display_result));
-      }
-
-      // Filter out view mode that are not enabled.
-      $view_modes = array_intersect_key($view_modes, array_flip($enabled_view_mode_ids));
-
-      // Get view mode labels.
-      $view_modes = array_map(function ($view_mode) {
-        return $view_mode['label'];
-      }, $view_modes);
-    }
-    catch (\Exception $e) {
-      watchdog_exception('elasticsearch_helper_content', $e);
-    }
-
-    return ['default' => t('Default')] + $view_modes;
   }
 
   /**
