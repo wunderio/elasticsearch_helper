f56ff05e1481cbb4539666add6b45b05674c57f6
Marc Galang <marc.galang@wunder.io>
2020-06-04 15:10:33 +0300

    Add Kernel test for node indexing. Fixed entity ops tests

--- tests/src/FunctionalJavascript/EntityOpsTest.php
+++ tests/src/FunctionalJavascript/EntityOpsTest.php
@@ -12,6 +12,8 @@ use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
  */
 class EntityOpsTest extends WebDriverTestBase {
 
+  protected $defaultTheme = 'stable';
+
   /**
    * {@inheritdoc}
    */
