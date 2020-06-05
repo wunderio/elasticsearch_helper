c20f39453339d5788c9f70133fde70a5f0c0a974
Raimonds Kalnins <raimonds.kalnins@wunder.io>
2020-06-05 19:03:17 +0300

    Index is created with explicit settings and mapping.

--- tests/modules/elasticsearch_helper_test/src/Plugin/ElasticsearchIndex/SimpleNodeIndex.php
+++ tests/modules/elasticsearch_helper_test/src/Plugin/ElasticsearchIndex/SimpleNodeIndex.php
@@ -13,4 +13,54 @@ use Drupal\elasticsearch_helper\Plugin\ElasticsearchIndexBase;
  *   entityType = "node"
  * )
  */
-class SimpleNodeIndex extends ElasticsearchIndexBase {}
+class SimpleNodeIndex extends ElasticsearchIndexBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setup() {
+    $index_name = $this->getIndexName([]);
+
+    if (!$this->client->indices()->exists(['index' => $index_name])) {
+      $this->client->indices()->create([
+        'index' => $index_name,
+        'body' => [
+          'number_of_shards' => 1,
+          'number_of_replicas' => 0,
+        ],
+      ]);
+
+      $mapping = $this->getMapping();
+      $this->client->indices()->putMapping($mapping);
+    }
+  }
+
+  /**
+   * Returns field mapping.
+   *
+   * @return array
+   */
+  protected function getMapping() {
+    return [
+      'index' => $this->getIndexName([]),
+      'type' => $this->getTypeName([]),
+      'body' => [
+        'properties' => [
+          'id' => [
+            'type' => 'keyword',
+          ],
+          'uuid' => [
+            'type' => 'keyword',
+          ],
+          'title' => [
+            'type' => 'text',
+          ],
+          'status' => [
+            'type' => 'keyword',
+          ],
+        ],
+      ],
+    ];
+  }
+
+}
