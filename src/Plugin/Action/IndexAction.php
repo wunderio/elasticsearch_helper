a777ae4c5abc59d055df5cf3bca56f6ac76106e7
Raimonds Kalnins <raimonds.kalnins@gmail.com>
2020-09-28 08:11:47 +0300

    Index action plugin added.

---
+++ src/Plugin/Action/IndexAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\elasticsearch_helper\Plugin\Action;
+
+/**
+ * Indexes entity into Elasticsearch.
+ *
+ * @Action(
+ *   id = "elasticsearch_helper_index",
+ *   action_label = @Translation("Index into Elasticsearch")
+ * )
+ */
+class IndexAction {
+
+}
