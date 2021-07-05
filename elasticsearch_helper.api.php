cf2b4a13e266d363311c9d97a6dd4534374959ad
Raimonds <maijs@users.noreply.github.com>
2021-07-05 14:52:54 +0300

    Hook "elasticsearch_helper_reindex_entity_query_alter" added to allow modules to alter entity query which selects the entities for reindexing. (#74)

--- elasticsearch_helper.api.php
+++ elasticsearch_helper.api.php
@@ -5,6 +5,8 @@
  * Hooks provided by the elasticsearch_helper module.
  */
 
+use \Drupal\Core\Entity\Query\QueryInterface;
+
 /**
  * @addtogroup hooks
  * @{
@@ -19,6 +21,21 @@
 function hook_elasticsearch_helper_client_builder_alter(\Elasticsearch\ClientBuilder $clientBuilder) {
   // Send log entries from the client directly to Drupal's log.
   $clientBuilder->setLogger(\Drupal::logger('elasticsearch'));
+}
+
+/**
+ * Alters the entity query which selects entities for reindexing.
+ *
+ * @param \Drupal\Core\Entity\Query\QueryInterface $query
+ *   Entity query instance.
+ * @param $entity_type
+ *   Type of entities which need to be re-indexed.
+ * @param string|null $bundle
+ *   Optional bundle name.
+ */
+function hook_elasticsearch_helper_reindex_entity_query_alter(QueryInterface $query, $entity_type, $bundle = NULL) {
+  // Do not restrict entity query based on user's permissions or node grants.
+  $query->accessCheck(FALSE);
 }
 
 /**
